a493e635290a4a8c3bdd8d37b152caff789a8ff8
MatanNahum <matan.nahum95@gmail.com>
2022-11-03 11:16:55 +0200

    spot check 1

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
-import React, { Component } from 'react';
-import './App.css';
-import Company from './components/Company'
-import Wardrobe from './components/Wardrobe';
-import Wardrobe2 from './components/Wardrobe2';
-import Calendar from './components/Calendar';
-import Register from './components/Register';
-import Hudini from './components/Hudini';
-import Home from './components/Home';
-import Landing from './components/Landing';
+import React, { Component } from "react";
+import "./App.css";
+import Company from "./components/Company";
+import Wardrobe from "./components/Wardrobe";
+import Wardrobe2 from "./components/Wardrobe2";
+import Calendar from "./components/Calendar";
+import Register from "./components/Register";
+import Hudini from "./components/Hudini";
+import Home from "./components/Home";
+import Landing from "./components/Landing";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       reservations: [
         { day: "Monday", time: 2000, name: "Earl" },
         { day: "Monday", time: 1845, name: "Ella" },
         { day: "Tuesday", time: 1930, name: "Linda" },
-        { day: "Wednesday", time: 2015, name: "Anni" }],
+        { day: "Wednesday", time: 2015, name: "Anni" },
+      ],
       user: "Robyn",
       store: [
         { item: "XSPS Pro Player", price: 800, discount: 0.2, hottest: false },
         { item: "Gizem Backwatch", price: 230, discount: 0.6, hottest: false },
-        { item: "Surround Sound Pelican", price: 3099, discount: 0.05, hottest: true }
+        {
+          item: "Surround Sound Pelican",
+          price: 3099,
+          discount: 0.05,
+          hottest: true,
+        },
       ],
       shouldDiscount: false,
-      currentPage: "Landing"
-    }
+      currentPage: "Landing",
+    };
   }
-  // SPOT-CHECK-2: 
-  //  your generateCompanyTags method here 
+  // SPOT-CHECK-2:
+  //  your generateCompanyTags method here
 
   // SPOT-CHECK-3:
   // your upperCase method here
 
   render() {
-
     let companies = [
       { name: "Tesla", revenue: 140 },
       { name: "Microsoft", revenue: 300 },
-      { name: "Google", revenue: 600 }]
-
-
+      { name: "Google", revenue: 600 },
+    ];
 
     return (
       <div>
-
         <div className="ex-space">
           <h4 className="ex-title">Spotcheck 1</h4>
           <div className="exercise" id="spotcheck-1">
-            {/* your code here */}
+            <Company name={companies[0].name}></Company>
           </div>
         </div>
 
@@ -122,9 +125,8 @@ class App extends Component {
             {/* your code here */}
           </div>
         </div>
-
       </div>
-    )
+    );
   }
 }
 
