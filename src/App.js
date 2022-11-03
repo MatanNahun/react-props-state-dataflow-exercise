ed690ee8622d9b9d618cbdcc8fdb555c83cea96d
MatanNahum <matan.nahum95@gmail.com>
2022-11-03 14:15:46 +0200

    exercise1

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ class App extends Component {
         <div className="ex-space">
           <h4 className="ex-title">Exercise 1</h4>
           <div className="exercise" id="ex-1">
-            {/* your code here */}
+            <Hudini></Hudini>
           </div>
         </div>
 
--- src/components/Hudini.js
+++ src/components/Hudini.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Hudini extends Component {
-    constructor() {
-        super()
-    }
-    render() {
-        return {/* your code here */ }
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+  }
 
-    }
+  render() {
+    let divMassage = this.state.show ? "Now you see me" : "Now you don’t";
+    return <div>{divMassage}</div>;
+  }
 }
 
-export default Hudini
+export default Hudini;
